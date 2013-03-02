73a8c37c1819ba2a18f5a2aa6e7901907454c74a
Peter Alfvin <palfvin@gmail.com>
2013-03-02 12:12:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Botmetrics::Application.config.secret_token = 'efde58e56a34304921f442f1ec11f20a331dff24515a42f1278b755b7cb487d0996a1f31653de7da670fbff88ffed2f31efc37b5a52992e7d6ab34df2794c29a'
