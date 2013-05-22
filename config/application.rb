daad858cdaa73aa192a1721d42122860fde147ea
Peter Alfvin <palfvin@gmail.com>
2013-05-22 13:21:12 -0500

    Asset precompile workaround

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Botmetrics
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Avoid precompile failure per Heroku troubleshooting guidelines (PWA)
+    config.assets.initialize_on_precompile = false
   end
 end
